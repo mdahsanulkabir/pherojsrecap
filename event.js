04232dbdf6b57b221fa5576d7de33e9fd24736f0
ahsan <makabir98@yahoo.com>
2022-07-12 14:02:37 +0600

    use button to create an element and take value from input and set the new elemvent innertext from the inupt text

--- event.js
+++ event.js
@@ -1,3 +1,24 @@
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
+
+const blueButton = document.getElementById('make-blue-button');
+// blueButton.onclick = makeBlueButton;
+// function makeBlueButton() {
+//     document.body.style.backgroundColor = 'blue';
+// }
+
+blueButton.onclick = () => document.body.style.backgroundColor = 'blue';
+
+const goldenButton = document.getElementById('golden-button');
+goldenButton.addEventListener('click' , () => {
+    document.body.style.backgroundColor = 'goldenrod';
+})
+
+document.getElementById('update-me').addEventListener('click', () =>{
+    const text = document.getElementById('put-text');
+    const p = document.createElement('p');
+    p.innerText = text.value;
+    const section = document.getElementById('section');
+    section.appendChild(p)
+})
